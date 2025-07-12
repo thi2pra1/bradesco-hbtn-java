e7a19efa88df199ed261efa12dcde651280776b8
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-07-12 18:49:18 -0300

    first commit

--- Interfaces_Exercicios/workflow/atividades/CodificarVideo.java
+++ Interfaces_Exercicios/workflow/atividades/CodificarVideo.java
@@ -1,3 +1,4 @@
+package atividades;
 public class CodificarVideo implements Atividade {
     public void executar() {
         System.out.println("codificando video");
--- Interfaces_Exercicios/workflow/atividades/EnviarEmail.java
+++ Interfaces_Exercicios/workflow/atividades/EnviarEmail.java
@@ -1,4 +1,4 @@
-
+package atividades;
 public class EnviarEmail implements Atividade {
     public void executar() {
         System.out.println("enviando email com video");
