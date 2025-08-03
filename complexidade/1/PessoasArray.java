f4ca92a6a42ffe573110e85f20700186bff436ad
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-08-03 17:09:33 -0300

    first commit

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -19,13 +19,20 @@ public class PessoasArray {
 
     public void buscaLinear(String nome) {
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
+        int posicaoEncontrada = -1;
+
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i] == nome) {
-                System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
-                return;
+                posicaoEncontrada = i;
             }
         }
-        throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+
+        if (posicaoEncontrada != -1) {
+            System.out.println("Nome pesquisado é " + nome + " que está na posição " + posicaoEncontrada);
+        } else {
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
     }
+
 }
