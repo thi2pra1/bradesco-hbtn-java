d451cb20554e682c0ba8d92e77654b99363f971d
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-08-03 18:38:46 -0300

    first commit

--- List/numeros/Program.java
+++ List/numeros/Program.java
@@ -4,10 +4,14 @@ public class Program {
     public static void main(String[] args) {
         List<Integer> numeros = new ArrayList<Integer>();
 
-        ManipularArrayNumeros.adicionarNumero(numeros, 1);
-        ManipularArrayNumeros.adicionarNumero(numeros, 3);
-        ManipularArrayNumeros.adicionarNumero(numeros, 6);
-        ManipularArrayNumeros.adicionarNumero(numeros, 9);
+        try {
+            ManipularArrayNumeros.adicionarNumero(numeros, 1);
+            ManipularArrayNumeros.adicionarNumero(numeros, 3);
+            ManipularArrayNumeros.adicionarNumero(numeros, 6);
+            ManipularArrayNumeros.adicionarNumero(numeros, 9);
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println(numeros);
 
@@ -17,8 +21,12 @@ public class Program {
             System.out.println(ex.getMessage());
         }
 
-        ManipularArrayNumeros.removerNumero(numeros, 3);
-        ManipularArrayNumeros.removerNumero(numeros, 9);
+        try {
+            ManipularArrayNumeros.removerNumero(numeros, 3);
+            ManipularArrayNumeros.removerNumero(numeros, 9);
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println(numeros);
 
@@ -29,11 +37,9 @@ public class Program {
         }
 
         ManipularArrayNumeros.substituirNumero(numeros, 3, 5);
-
         System.out.println(numeros);
 
         ManipularArrayNumeros.substituirNumero(numeros, 5, 3);
-
         System.out.println(numeros);
     }
 }
