4b6f988e4e06e3cc133adbc1d722b84d237ef571
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-08-03 17:11:56 -0300

    first commit

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -23,7 +23,7 @@ public class PessoasArray {
 
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
-            if (nomes[i] == nome) {
+            if (nomes[i].equals(nome)) {
                 posicaoEncontrada = i;
             }
         }
