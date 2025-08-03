12724639ab023d615f50fa60d8e1966cab8322ef
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-08-03 17:31:13 -0300

    first commit

--- complexidade/1/Program.java
+++ complexidade/1/Program.java
@@ -2,13 +2,15 @@ public class Program {
 
     public static void main(String[] args) {
         long inicio = System.nanoTime();
+        PessoasArray p = new PessoasArray();
+
         try {
-            PessoasArray p = new PessoasArray();
             p.buscaLinear("Otavio");
             p.buscaLinear("Isabela");
             p.buscaLinear("Anderson");
-        } finally {
+        } catch (IllegalArgumentException e) {
             System.out.println("Tempo de execução: " + (System.nanoTime() - inicio));
+            throw e;
         }
     }
 }
