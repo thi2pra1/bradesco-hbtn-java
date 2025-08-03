7cd95533d2d49a99a71e3f82790ae17cd4ec32b7
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-08-03 18:50:03 -0300

    first commit

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Blog {
+    private List<Post> postagens;
+
+    public Blog() {
+        this.postagens = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post postagem) {
+        postagens.add(postagem);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> autores = new TreeSet<>();
+        for (Post p : postagens) {
+            autores.add(p.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        Map<String, Integer> contagem = new TreeMap<>();
+        for (Post p : postagens) {
+            contagem.put(p.getCategoria(), contagem.getOrDefault(p.getCategoria(), 0) + 1);
+        }
+        return contagem;
+    }
+}
