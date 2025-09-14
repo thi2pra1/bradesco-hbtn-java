dd3180a73be14f2e47be7064c346678aa8f645cb
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-09-14 16:31:05 -0300

    first commit

---
+++ Streams_Collector_Optional_Exercicios/ConsultaPessoas.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)
+                ));
+    }
+}
