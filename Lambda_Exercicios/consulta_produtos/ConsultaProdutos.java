c231628512dad192bf5fa5ba0497db72972f721e
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-09-14 15:37:16 -0300

    first commit

---
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -0,0 +1,15 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterio) {
+        List<Produto> filtrados = new ArrayList<>();
+        for (Produto produto : produtos) {
+            if (criterio.testar(produto)) {
+                filtrados.add(produto);
+            }
+        }
+        return filtrados;
+    }
+}
