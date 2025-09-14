82f5cd55a04031b1f76423327912382fcca3e9ea
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-09-14 16:20:35 -0300

    first commit

---
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+}
