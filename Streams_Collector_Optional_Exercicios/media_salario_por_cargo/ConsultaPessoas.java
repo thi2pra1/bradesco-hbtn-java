38150f129ab33f24311d97422e4a012b1d56cc9f
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-09-14 18:02:57 -0300

    first commit

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,13 @@
+java
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.averagingDouble(p -> p.getSalario())
+                ));
+    }
+}
