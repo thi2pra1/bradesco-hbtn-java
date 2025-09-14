1ce406f3297918db16f37e38962a814165acacae
Thiago Carneiro <thiagoc.silva@bradesco.com.br>
2025-09-14 16:35:47 -0300

    first commit

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/Pessoa.java
@@ -0,0 +1,33 @@
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario)
+                .replace('.', ',');
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        int cmp = this.nome.compareTo(o.nome);
+        if (cmp != 0) return cmp;
+        return Integer.compare(this.codigo, o.codigo);
+    }
+}
